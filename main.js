c11acb826e6cc6f0a3879477787ce7a1ed6972cd
Hugo Pat <Hugo.Paternoster@hotmail.com>
2025-03-25 16:51:46 +0100

    add some function to the site

--- main.js
+++ main.js
@@ -8,26 +8,65 @@ authorDiv.classList.add("author");
 container.appendChild(quoteDiv);
 container.appendChild(authorDiv);
 
+const ageAuthorDiv = document.createElement("span");
+ageAuthorDiv.classList.add("age-author");
+container.appendChild(ageAuthorDiv);
+
 function display(text, div) {
-  div.innerHTML = text;
+  div.textContent = text;
 }
 
 async function quote() {
+  const loader = document.getElementById("loader");
+  loader.style.display = "block";
+  container.style.display = "none";
   try {
-    const response = await fetch("https://thatsthespir.it/api");
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json();
+    // await new Promise((resolve) => setTimeout(resolve, 2000));
 
-    display(data.quote, quoteDiv);
-    display(data.author, authorDiv);
+    container.style.display = "block";
+    const responseQuote = await fetch("https://thatsthespir.it/api");
+    if (!responseQuote.ok) {
+      throw new Error(`HTTP error! Status: ${responseQuote.status}`);
+    }
+    const dataQuote = await responseQuote.json();
+
+    loader.style.display = "none";
+
+    display(dataQuote.quote, quoteDiv);
+    display(dataQuote.author, authorDiv);
   } catch (error) {
     console.log(error);
+    container.style.display = "block";
+    loader.style.display = "none";
     display(`Error : ${error}`, quoteDiv);
+  }
+}
+
+async function ageAuthor() {
+  try {
+    const nameAuthor = authorDiv.textContent.split(" ");
+    const responseName = await fetch(
+      "https://api.agify.io/?name=" + nameAuthor[0]
+    );
+    if (!responseName.ok) {
+      throw new Error(`HTTP error ! Status ${responseName.status}`);
+    }
+    const dataName = await responseName.json();
+
+    display(dataName.age, ageAuthorDiv);
+  } catch (e) {
+    console.log(e);
+    loader.style.display = "none";
+
+    display(e, ageAuthorDiv);
   }
 }
 
 window.addEventListener("load", quote);
 
 myBtn.addEventListener("click", quote);
+authorDiv.addEventListener("click", ageAuthor);
+
+ageAuthorDiv.addEventListener("click", (e) => {
+  ageAuthorDiv.textContent = "";
+});
